a1344ba4944ec1ba67ef505132e08ccad8a459a8
Barbie <barbie@missbarbell.co.uk>
2014-08-16 10:05:35 +0000

    Import of BARBIE/CPAN-Testers-WWW-Statistics-Excel-0.05 from CPAN.
    gitpan-cpan-distribution: CPAN-Testers-WWW-Statistics-Excel
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         BARBIE/CPAN-Testers-WWW-Statistics-Excel-0.05.tar.gz
    gitpan-cpan-author:       BARBIE
    gitpan-cpan-maturity:     released

--- lib/CPAN/Testers/WWW/Statistics/Excel.pm
+++ lib/CPAN/Testers/WWW/Statistics/Excel.pm
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 use vars qw($VERSION);
 
-$VERSION = '0.04';
+$VERSION = '0.05';
 
 #----------------------------------------------------------------------------
 
@@ -305,7 +305,7 @@ F<http://blog.cpantesters.org/>
 
 =head1 COPYRIGHT AND LICENSE
 
-  Copyright (C) 2009-2013 Barbie for Miss Barbell Productions.
+  Copyright (C) 2009-2014 Barbie for Miss Barbell Productions.
 
   This distribution is free software; you can redistribute it and/or
   modify it under the Artistic Licence v2.
